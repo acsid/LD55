b17bfc62b4ded4dd4c27b580b85d49ebce9c5aa4
Vikingman <drake@popalot.net>
2024-04-14 20:24:43 -0400

    version 1.0 the version posted on ludumdare 55

--- Cast.gd
+++ Cast.gd
@@ -5,10 +5,12 @@ var cast = 0
 var casting = true
 var castTime = 100
 var castMulti = 1
-var casttype = preload("res://summon.tscn")
+@export var casttype = preload("res://summon.tscn")
+
+var castwhat = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Panel/castTarget.value = castTarget
+	$VBoxContainer/castTarget.value = castTarget
 	pass # Replace with function body.
 
 
@@ -23,11 +25,15 @@ func _process(delta):
 			cast -= castMulti 
 			if cast < 1:
 				up = true
-		$Panel/castProgress.value = cast
+		$VBoxContainer/castProgress.value = cast
 	if Input.is_action_just_pressed("cast"):
+		
 		casting = false
 		castTime -= abs(cast - castTarget)
 		print( castTime )
+		if castwhat == 2:
+			print("firesummon")
+			casttype = load("res://summon/firesummon.tscn")
 		var summon = casttype.instantiate()
 		summon.expire = castTime
 		summon.global_position = get_parent().global_position
